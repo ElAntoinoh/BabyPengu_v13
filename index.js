9ded90b2ce30d060a5306498fd5c826a19ecce20
ElAntoinoh <galopinantoine.contact@gmail.com>
2022-03-06 22:25:29 +0100

    Ajout de logs de chargements d'évènements et commandes + Ajout de vérifications de process

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ client.commands = new Collection();
 
 ['EventUtil', 'CommandUtil'].forEach(handler => { require(`./utils/handlers/${handler}`)(client); })
 
+process.on('exist'             , (code           ) => { console.log(`Le processus s'est arrêté avec le code: ${code}`   ); });
+process.on('uncaughtException' , (error, origin  ) => { console.log(`UNCAUGHT_EXCEPTION: ${error}`, `Origine: ${origin}`); });
+process.on('unhandledRejection', (reason, promise) => { console.log(`UNHANDLED_REJECTION: ${reason}\n-----\n`, promise  ); });
+process.on('warning'           , (...args        ) => { console.log(...args                                             ); });
+
 client.login(process.env.DISCORD_TOKEN);
--- utils/handlers/CommandUtil.js
+++ utils/handlers/CommandUtil.js
@@ -7,6 +7,11 @@ module.exports = async client => {
     (await pGlob(`${process.cwd()}/commands/*/*.js`)).map(async commandFile => {
         const command = require(commandFile);
 
+        if( !command.name )
+            return console.log(`-----\nCommande non chargée: pas de nom\nFichier -> ${commandFile}\n-----`);
+
         client.commands.set(command.name, command);
+
+        console.log(`Commande chargée: ${command.name}`);
     });
 };
