c7512633e9adb36ced15d260b0222b697092f914
ElAntoinoh <galopinantoine.contact@gmail.com>
2022-03-06 21:38:30 +0100

    Ajout d'un handler pour les events

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ dotenv.config();
 
 const client = new Client({ intents: 1 });
 
-client.once('ready', () => {
-    console.log('Je suis prêt !');
-});
+require('./utils/handlers/EventUtil')(client);
 
 client.login(process.env.DISCORD_TOKEN);
